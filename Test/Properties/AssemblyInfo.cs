e7e034ba67dc5282296af06de4d9b671aa11d598
mpmxyz <mpmxyz@users.noreply.github.com>
2023-11-06 00:51:25 +0100

    fixed NuGet package handling

--- Test/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("dab2d7a9-757b-4876-b95b-64a2e4f965b6")]
