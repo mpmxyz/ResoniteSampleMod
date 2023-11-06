17242ce6469694dd52e632795769146ac7f2a244
mpmxyz <mpmxyz@users.noreply.github.com>
2023-11-06 21:01:32 +0100

    Added support for MonkeyLoader

---
+++ SampleModMonkey.cs
@@ -0,0 +1,54 @@
+﻿
+using MonkeyLoader.Configuration;
+using MonkeyLoader.Resonite;
+using System;
+
+namespace SampleMod
+{//TODO: ensure that nuget pack -Build does not end in infinite recursion
+    public class SampleModMonkey : ResoniteMonkey<SampleModMonkey>, ISampleMod
+    {
+        public override string Name => "SampleMod";
+
+        private SampleModMonkeyConfig LoadedConfig;
+
+        public bool Enabled => LoadedConfig.Enabled.GetValue();
+
+        public void DoSomething()
+        {
+            Logger.Warn(() => "Hello World!");
+        }
+
+        protected override bool OnEngineReady()
+        {
+            LoadedConfig = Config.LoadSection<SampleModMonkeyConfig>();
+            Patches.Apply(this);
+            return base.OnEngineReady();
+        }
+
+        protected override void OnEngineShutdownRequested(string reason)
+        {
+            base.OnEngineShutdownRequested(reason);
+        }
+
+        protected override bool OnLoaded()
+        {
+            return base.OnLoaded();
+        }
+
+        protected override bool OnShutdown()
+        {
+            return base.OnShutdown();
+        }
+
+        private class SampleModMonkeyConfig : ConfigSection
+        {
+            public DefiningConfigKey<bool> Enabled = new DefiningConfigKey<bool>("Enabled", "Enables a small message on each button click.", () => true);
+
+            public override string Description => "MonkeyLoader flavor of sample mod's config";
+
+            public override string Name => "SampleMod";
+
+            public override Version Version => new Version(1, 0, 0);
+        }
+    }
+}
--- SampleModRML.cs
+++ SampleModRML.cs
@@ -1,20 +1,17 @@
-﻿using Elements.Core;
-using FrooxEngine;
-using FrooxEngine.UIX;
-using HarmonyLib; // HarmonyLib comes included with a ResoniteModLoader install
-using ResoniteModLoader;
+﻿using ResoniteModLoader;
 
 namespace SampleMod
 {
     /// <summary>
     /// This mod is an implementation based on the example given in https://github.com/resonite-modding-group/ResoniteModLoader/blob/main/doc/making_mods.md.
     /// </summary>
-    public class SampleMod : ResoniteMod
+    public class SampleModRML : ResoniteMod//, ISampleMod
     {
         public override string Name => "SampleMod";
         public override string Author => "mpmxyz";
         public override string Version => "1.0.0"; //Version of the mod, should match the AssemblyVersion
         public override string Link => "https://github.com/mpmxyz/ResoniteSampleMod";
+
 
         //The following
         [AutoRegisterConfigKey]
@@ -22,12 +19,13 @@ namespace SampleMod
 
         private static ModConfiguration Config;//If you use config settings, this will be where you interface with them
 
+        public bool Enabled => Config.GetValue(enabled);
+
         public override void OnEngineInit()
         {
             Config = GetConfiguration(); //Get this mods' current ModConfiguration
             Config.Save(true); //If you'd like to save the default config values to file
-            Harmony harmony = new Harmony("com.github.mpmxyz.SampleMod"); //typically a reverse domain name is used here (https://en.wikipedia.org/wiki/Reverse_domain_name_notation)
-            harmony.PatchAll(); // do whatever LibHarmony patching you need, this will patch all [HarmonyPatch()] instances
+            //Patches.Apply(this);
 
             //Various log methods provided by the mod loader, below is an example of how they will look
             //3:14:42 AM.069 ( -1 FPS)  [INFO] [ResoniteModLoader/SampleMod] a regular log
@@ -37,22 +35,9 @@ namespace SampleMod
             Error("an error log");
         }
 
-        //Example of how a HarmonyPatch can be formatted
-        [HarmonyPatch(typeof(Button), "OnPressBegin")]
-        class ClassName_MethodName_Patch
+        public void DoSomething()
         {
-            //Postfix() here will be automatically applied as a PostFix Patch
-            [HarmonyPostfix]
-            static void Postfix(Button __instance, Canvas.InteractionData eventData)
-            {
-                if (!Config.GetValue(enabled))
-                {//Use Config.GetValue() to use the ModConfigurationKey defined earlier
-                    return; //In this example if the mod is not enabled, we'll just return before doing anything
-                }
-                Warn("Hello World!");
-                FrooxEngineBootstrap.LogStream.Flush();
-                //Do stuff after everything in the original OnPressBegin has run.
-            }
+            Warn("Hello World!");
         }
     }
 }
