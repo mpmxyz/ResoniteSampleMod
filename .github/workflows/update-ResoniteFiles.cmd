351fc491542169cd294580e4226c481f2ba640d7
mpmxyz <mpmxyz@users.noreply.github.com>
2024-01-11 23:31:57 +0100

    Now Downloading Resonite from Steam (#47)
    removed secret RESONITE_CLONE_TOKEN
    added secrets STEAM_USER and STEAM_TOKEN
    closes #44

--- .github/workflows/update-ResoniteFiles.cmd
+++
@@ -1,6 +0,0 @@
-:: Note: This command needs the %ResonitePath% to be set! (quick & dirty code: does not check that)
-rmdir /S /Q Resonite
-mkdir Resonite\Resonite_Data\Managed\
-powershell -Command Copy -Destination 'Resonite' -Path '%ResonitePath%\*.dll' -Exclude 'MonkeyLoader.dll','Mono*','NuGet*','winhttp.dll','Zio.dll','0Harmony.dll'
-powershell -Command Copy -Destination 'Resonite\Resonite_Data\Managed\' -Path '%ResonitePath%\Resonite_Data\Managed\*.dll'
-:: powershell -Command Compress-Archive -Force -DestinationPath Resonite.zip -Path Resonite
